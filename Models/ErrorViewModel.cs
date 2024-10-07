61373489a8cbe3b02c90471208c07b035b858aa7
Rafael Thomaz <rafael.hthomaz123@gmail.com>
2024-10-07 15:52:20 -0300

    portifolio-rafa

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace portifolio_rafa.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
